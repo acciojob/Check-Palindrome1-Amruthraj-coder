c7d902a9e70b9224a1432912848b4104c986b75f
Amruthraj-coder <122180037+Amruthraj-coder@users.noreply.github.com>
2023-05-14 18:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+let arr=str.split("");
+let isapalindrome = true;
+arr = arr.filter(function(e){
+	return e!=" ";
+})
+	str=arr.join("");
+	let i=0,j=str.length-1;
+  while(i<=j){
+   if(str.charAt(i)!=str.charAt(j)){
+	   isapalindrome = false;
+   }
+  }
+	return isapalindrome;
 }
 module.exports = palindrome
